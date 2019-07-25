2f8e20cf59b68c36b38505e96f62c62821b58ff3
Abdukhalilov Bobur <destbest94@gmail.com>
2019-07-25 14:53:57 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,253 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app" class="mb-5">
+    <div>
+      <h3>
+        {{ expenses }}
+      </h3>
+    </div>
+    <div class="container">
+      <div class="jumbotron text-center">
+        <h1>Расходы и доходы домашних хозяйств</h1>
+      </div>
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="card mb-2">
+            <div class="alert alert-success m-0">Доход</div>
+            <div class="card-body">
+              <div class="mb-2">
+                <div class="row">
+                  <div class="col-sm-8">
+                    <label for="message">Напоминание:</label>
+                    <input 
+                      type="text"
+                      class="form-control form-control" 
+                      id="message"
+                      v-model="income.message"
+                    >
+                  </div>
+                  <div class="col-sm-4">
+                    <label for="summ">Сумма:</label>
+                    <input
+                      type="text"
+                      class="form-control form-control"
+                      id="summ"
+                      v-model="income.summ"
+                    >
+                  </div>
+                </div>
+                
+              </div>
+              <button type="button" class="btn btn-success" @click="onAddIncome()">Добавить</button>
+            </div> 
+          </div>
+          <div class="card mb-2">
+            <div class="alert alert-warning m-0">Затраты</div>
+            <div class="card-body">
+              <div class="mb-2">
+                <div class="row">
+                  <div class="col-sm-8">
+                    <label for="e_message">Напоминание:</label>
+                    <input type="text" class="form-control form-control" id="e_message" v-model="expenses.message">
+                  </div>
+                  <div class="col-sm-4">
+                    <label for="e_summ">Сумма:</label>
+                    <input type="text" class="form-control form-control" id="e_summ" v-model="expenses.summ">
+                  </div>
+                </div>
+              </div>
+              <button type="button" class="btn btn-success" @click="onAddExpenses()">Добавить</button>
+            </div> 
+          </div>
+        </div>
+        <div class="col-sm-8">
+          <div>
+            <!-- The Modal -->
+            <div class="modal fade" id="myModal">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                
+                  <!-- Modal Header -->
+                  <div class="modal-header">
+                    <h4 v-if="target.type" class="modal-title">Доход</h4>
+                    <h4 v-else class="modal-title">Затраты</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  </div>
+                  
+                  <!-- Modal body -->
+                  <div class="modal-body">
+                    <div class="row">
+                      <div class="col-sm-8">
+                        <label for="message">Напоминание:</label>
+                        <input 
+                          type="text"
+                          class="form-control form-control" 
+                          id="message"
+                          ref="message"
+                          v-model="target.message">
+                      </div>
+                      <div class="col-sm-4">
+                        <label for="summ">Сумма:</label>
+                        <input
+                          type="text"
+                          class="form-control form-control"
+                          id="summ"
+                          ref="summ"
+                          v-model="target.summ">
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <!-- Modal footer -->
+                  <div class="modal-footer">
+                    <button 
+                      type="button"
+                      class="btn btn-warning" 
+                      data-dismiss="modal"
+                      @click="onChange()"
+                    >Change</button>
+
+                    <button 
+                      type="button"
+                      class="btn btn-dark"
+                      data-dismiss="modal"
+                      @click="onDelete()"
+                    >Delete</button>
+
+                    <button
+                      type="button"
+                      class="btn btn-danger"
+                      data-dismiss="modal"
+                    >Close</button>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+
+            <div class="container p-0">
+              <div class="row">
+                <div class="col">
+                  <div class="alert alert-success text-center">
+                    <div>Доход</div>
+                    <div>{{ getAllSumm.income }}</div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="alert alert-warning text-center">
+                    <div>Затраты</div>
+                    <div>{{ getAllSumm.expenses }}</div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="alert alert-primary text-center">
+                    <div>Баланс</div>
+                    <div>{{ getAllSumm.balance }}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="container p-0">
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="list-group">
+                    <button 
+                      v-for="item in items"
+                      :key="item.id"
+                      type="button" 
+                      class="list-group-item list-group-item-action"
+                      :class="[ item.type ? 'list-group-item-success' : 'list-group-item-warning']"
+                      @click="openDialog(item)"
+                      data-toggle="modal" data-target="#myModal"
+                    >
+                      <span>
+                        {{ item.message }}
+                      </span>
+                      <div class="float-sm-right">
+                        {{ item.summ }}
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      income: { message: "", summ: "", type: 1 },
+      expenses: { message: "", summ: "", type: 0 },
+      target: { id: 0, message: "", summ: 0, type: 1 },
+      items: [],
+    }
+  },
+  computed: {
+    getAllSumm() {
+      let income = 0;
+      let expenses = 0; 
+      let balance = 0;
+      this.items.forEach((item) => {
+        if (item.type) {
+          income += (item.summ - 0);
+        } else {
+          expenses += (item.summ - 0);
+        }
+      });
+
+      balance = income - expenses;
+
+      return { income, expenses, balance };
+    }
+  },
+  methods: {
+    openDialog(item) {
+      this.target = item;
+    },
+
+    async onChange() {
+      let id = this.target.id;
+      let res = await this.$services.messages.patch(id, this.target);
+      this.getAllItems();
+    },
+
+    async onDelete() {
+      await this.$services.messages.remove(this.target.id);
+      this.getAllItems();
+    },
+
+    async onAddIncome() {
+      let res = await this.$services.messages.create(this.income);
+      this.getAllItems();
+      this.income.message = "";
+      this.income.summ = "";
+    },
+
+    async onAddExpenses() {
+      let res = await this.$services.messages.create(this.expenses);
+      this.getAllItems();
+      this.expenses.message = "";
+      this.expenses.summ = "";
+    },
+
+    async getAllItems() {
+      let res = await this.$services.messages.find();
+      this.items = res;
+    }
+  },
+  mounted() {
+    this.getAllItems();
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+
+const Feathers = require('feathers/client')
+const hooks = require('feathers-hooks')
+const socketio = require('feathers-socketio/client')
+const io = require('socket.io-client')
+
+const socket = io('http://localhost:3030/')
+const feathers = Feathers()
+.configure(socketio(socket))
+.configure(hooks())
+
+feathers.service('messages');
+
+const vueFeathers = require('vue-feathers')
+
+Vue.use(vueFeathers, feathers)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
